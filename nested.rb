76c034da12f841d53494eaefac835b3a2f1c78d3
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-04 22:01:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,12 +83,12 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
 end
 
 def changing_alan
-	programmer_hash = {:known_for => "Ruby", :languages => ["LISP", "C"]
-# }
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
